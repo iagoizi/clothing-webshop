13fb2737fd8b8e0ee16168ba96374341f9a069d4
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2023-06-22 17:11:31 +0200

    Renamed pics from homepage

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,17 @@
+var menuBtn = document.getElementById("menuBtn");
+var menu = document.getElementById("menu");
+var sideNav = document.getElementById("sideNav");
 
-var menuBtn=document.getElementById("menuBtn")
-var menu=document.getElementById("menu")
-var sideNav=document.getElementById("sideNav")
-
-menuBtn.onclick=function(){
-    if(sideNav.style.right== "-200px" ){
-        sideNav.style.right= "0"
-        menu.src="../photo/close.png";
-    }
-    else{
-        sideNav.style.right= "-200px"
-        menu.src="../photo/menu.png";
-    }
-}
+menuBtn.onclick = function () {
+  if (sideNav.style.right == "-200px") {
+    sideNav.style.right = "0";
+    menu.src = "../img/common/close.png";
+  } else {
+    sideNav.style.right = "-200px";
+    menu.src = "../img/common/menu.png";
+  }
+};
 var scroll = new SmoothScroll('a[href*="#"]', {
-speed: 1000,
-speedAsDuration: true
+  speed: 1000,
+  speedAsDuration: true,
 });
