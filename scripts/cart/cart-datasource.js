2413e607d46b724d9d6f74114696e1895fc3f403
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2023-06-28 11:04:55 +0200

    Add fake data

--- scripts/cart/cart-datasource.js
+++ scripts/cart/cart-datasource.js
@@ -17,4 +17,32 @@ const FAKE_DATA = [
     pricePerUnit: 10,
     quantity: 1,
   },
+  {
+    id: "product-3",
+    description: "T-shirt",
+    thumbnail: "https://i.imgur.com/jibvF37.jpg",
+    pricePerUnit: 10,
+    quantity: 1,
+  },
+  {
+    id: "product-4",
+    description: "Saracura Pants",
+    thumbnail: "https://i.imgur.com/xg7udSL.jpg",
+    pricePerUnit: 10,
+    quantity: 1,
+  },
+  {
+    id: "product-5",
+    description: "Star Girl Hoodie",
+    thumbnail: "https://i.imgur.com/HtC01cF.png",
+    pricePerUnit: 10,
+    quantity: 1,
+  },
+  {
+    id: "product-6",
+    description: "Winx boy Hoodie",
+    thumbnail: "https://i.imgur.com/5eAPpoV.jpg",
+    pricePerUnit: 10,
+    quantity: 1,
+  },
 ];
